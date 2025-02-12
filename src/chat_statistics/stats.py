709399e2f234f841c7ba55629af2267cf7283c60
Erfan_Fazel <erfanfazel1389@gmail.com>
2025-02-12 11:44:26 +0330

    add users functionality

--- src/chat_statistics/stats.py
+++ src/chat_statistics/stats.py
@@ -3,11 +3,12 @@ from pathlib import Path
 import json
 from src.data import DATA_DIR
 from loguru import logger
-from hazm import word_tokenize,Normalizer,normalizer
+from hazm import word_tokenize,Normalizer,sent_tokenize
 import arabic_reshaper
 from bidi.algorithm import get_display
 from wordcloud import WordCloud
-
+from collections import Counter,defaultdict
+from src.utils.io import read_file,read_json
 
 
 class ChatStatistics:
@@ -19,15 +20,58 @@ class ChatStatistics:
         """param chat_json: path to telegram export json file 
         """
         logger.info(f"loading chat data from {chat_json}")
-        with open(Path(chat_json)) as f:
-            self.chat_json = json.load(f)
+        
+        self.chat_json = read_json(Path(chat_json))
         self.normalizer  = Normalizer() 
         #load stop words 
         logger.info(f"loading stop wrods from {DATA_DIR  / 'stopwords.txt'}")  
-        stop_words = open(DATA_DIR / 'stop_words.txt').readlines()
+        stop_words = read_file(DATA_DIR / 'stop_words.txt')
         stop_words = list(map(str.strip, stop_words))
-        self.stop_words = list(map(self.normalizer.normalize,stop_words))    
+        self.stop_words = set(map(self.normalizer.normalize,stop_words))    
+    @staticmethod
+    def rebuild_msg(sub_messages):
+        msg_text = ''
+        for sub_msg in sub_messages:
+            if isinstance(sub_msg, str):
+                msg_text += sub_msg 
+            elif 'text' in sub_msg:
+                msg_text += sub_msg['text']
+        return msg_text         
+    def msg_has_question(self,msg): 
+        """Checks if a message has a question
+        """
+        if not isinstance(msg['text'],str):
+            msg['text'] = self.rebuild_msg(msg['text'])
+            sentences = sent_tokenize(msg["text"])
+            for sentence in sentences:
+                if('?' not in sentence) and ('؟' not in sentence):
+                    continue 
+                return True
 
+    def get_top_users(self, top_n:int = 10):
+        #chack messages for question
+        is_question = defaultdict(bool)
+        for msg in self.chat_json['messages']:
+            if not  isinstance(msg['text'],str):
+                msg['text'] = self.rebuild_msg(msg['text'])
+            sentences = sent_tokenize(msg["text"])
+            for sentence in sentences:
+                if('?' not in sentence) and ('؟' not in sentence):
+                    continue 
+                
+                is_question[msg['id']] = True
+                break
+    
+        logger.info("Getting top users...")
+        users = []
+        for msg in self.chat_json["messages"]:
+            if not msg.get('reply_to_message_id'):
+                continue
+            #users[msg['from_id']].append(msg["reply_to_message_id"])   
+            if not is_question[msg["reply_to_message_id"]] is False:
+                continue
+            users.append(msg["from"])    
+        return dict(Counter(users).most_common(10))
     def generate_word_cloud(self,output_dir):
         """Generats word cloud of the chat data
         param output_dir: path to output directory for word cloud image
@@ -51,3 +95,5 @@ class ChatStatistics:
 if __name__ == "__main__":
     chat_stats = ChatStatistics(chat_json=DATA_DIR / 'result.json')
     chat_stats.generate_word_cloud(output_dir=DATA_DIR)
+    top_users = chat_stats.get_top_users(top_n=10)
+    print(top_users)
